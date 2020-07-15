4a8602876a5103a4bc1765d47e04c0b806d3e4cd
hatmaker <riteshx82@gmail.com>
2020-07-15 05:36:39 -0700

    MVP Ready

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
-var multer = require('multer');
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/fileanalyse', (req,res) => {
+app.get("/hello", function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
 
-  console.log(req);
-  res.send('post form clicked');
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (req.file) {
+    res
+      .status(200)
+      .json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+      });
+  } else {
+    res.send("No file uploaded!");
+  }
 });
 
 app.listen(port, function () {
-  console.log('Node.js listening: '+port);
+  console.log("Node.js listening: " + port);
 });
